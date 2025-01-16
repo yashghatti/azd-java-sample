70e22a530f3b24fc809c11e5c37151ec56485baf
Yash Ghatti <yash.ghatti@ehealthsask.ca>
2025-01-16 17:38:56 -0600

    initial commit

---
+++ src/main/java/ca/yashghatti/sbt/SbtApplication.java
@@ -0,0 +1,13 @@
+package ca.yashghatti.sbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/yashghatti/sbt/SbtApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.yashghatti.sbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
